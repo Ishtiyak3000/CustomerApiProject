f95f36dd3934bb825525a011d0a76f41c52feab3
Ishtiyak3000 <khanishtiyak9721@gmail.com>
2024-11-02 00:04:35 +0530

    starting project

--- src/main/java/com/mailverifier/customerapiproject/CustomerApiProjectApplication.java
+++ src/main/java/com/mailverifier/customerapiproject/CustomerApiProjectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CustomerApiProjectApplication {
-
+    //hello
     public static void main(String[] args) {
         SpringApplication.run(CustomerApiProjectApplication.class, args);
     }
