b8beefac42bb4fe24e26ba47d76b77a858d2a3f8
Ishtiyak3000 <khanishtiyak9721@gmail.com>
2024-11-02 00:17:07 +0530

    created customer

--- src/main/java/com/mailverifier/customerapiproject/controller.java
+++ src/main/java/com/mailverifier/customerapiproject/controller.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/customer")
 public class controller {
+
     private final CustomerService customerService;
 
     @PostMapping
