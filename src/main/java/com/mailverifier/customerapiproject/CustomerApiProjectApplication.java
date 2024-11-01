30e41b0b47db761b7d84d50aef5e3a40b0e05d0a
Ishtiyak3000 <khanishtiyak9721@gmail.com>
2024-11-02 00:14:44 +0530

    created customer

--- src/main/java/com/mailverifier/customerapiproject/CustomerApiProjectApplication.java
+++ src/main/java/com/mailverifier/customerapiproject/CustomerApiProjectApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CustomerApiProjectApplication {
-    //hellohh
-    //checking the commit
+
     public static void main(String[] args) {
         SpringApplication.run(CustomerApiProjectApplication.class, args);
     }
