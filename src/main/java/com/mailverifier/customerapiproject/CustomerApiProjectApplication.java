76a32c200a1ae77149d67628143f56a71a014a3e
Ishtiyak3000 <khanishtiyak9721@gmail.com>
2024-11-02 00:07:19 +0530

    second commit

--- src/main/java/com/mailverifier/customerapiproject/CustomerApiProjectApplication.java
+++ src/main/java/com/mailverifier/customerapiproject/CustomerApiProjectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CustomerApiProjectApplication {
     //hellohh
+    //checking the commit
     public static void main(String[] args) {
         SpringApplication.run(CustomerApiProjectApplication.class, args);
     }
