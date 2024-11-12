7a4810a1f22ab62f398f5b6c6b2849fe8c57db49
Ishtiyak3000 <khanishtiyak9721@gmail.com>
2024-11-13 00:44:26 +0530

    login handeler modification

--- src/main/java/com/mailverifier/customerapiproject/controller/loginhandler.java
+++ src/main/java/com/mailverifier/customerapiproject/controller/loginhandler.java
@@ -1,4 +1,12 @@
 package com.mailverifier.customerapiproject.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class loginhandler {
+    @GetMapping
+   public String login() {
+        return "login";
+    }
+
+
 }
